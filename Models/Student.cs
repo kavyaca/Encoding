d1cb9876a4fbf2e6918bc6c58a818421982f4853
kavya666 <ruhaansodhi29@gmail.com>
2020-03-24 03:32:55 -0400

    Initial check-in of module CSV

---
+++ Models/Student.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSV.Models
+{
+    public class Student
+    {
+        public string StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private string _DateOfBirth;
+        public string DateOfBirth
+        {
+            get { return _DateOfBirth; }
+            set
+            {
+                _DateOfBirth = value;
+
+                //Convert DateOfBirth to DateTime
+                DateTime dtOut;
+                DateTime.TryParse(_DateOfBirth, out dtOut);
+                DateOfBirthDT = dtOut;
+            }
+        }
+
+        public DateTime DateOfBirthDT { get; internal set; }
+        public string ImageData { get; set; }
+
+        public string AbsoluteUrl { get; set; }
+        public string Directory { get; set; }
+        public string FullPathUrl
+        {
+            get
+            {
+                return AbsoluteUrl + "/" + Directory;
+            }
+        }
+
+        public void FromCSV(string csvdata)
+        {
+            string[] data = csvdata.Split(",", StringSplitOptions.None);
+
+            StudentId = data[0];
+            FirstName = data[1];
+            LastName = data[2];
+            DateOfBirth = data[3];
+            ImageData = data[4];
+        }
+
+        public void FromDirectory(string directory)
+        {
+            Directory = directory;
+
+            if(String.IsNullOrEmpty(directory.Trim()))
+            {
+                return;
+            }
+
+            string[] data = directory.Trim().Split(" ", StringSplitOptions.None);
+
+            StudentId = data[0];
+            FirstName = data[1];
+            LastName = data[2];
+        }
+
+        public string ToCSV()
+        {
+            string result = $"{StudentId},{FirstName},{LastName},{DateOfBirthDT.ToShortDateString()},{ImageData}";
+            return result;
+        }
+
+        public override string ToString()
+        {
+            string result = $"{StudentId} {FirstName} {LastName}";
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using CSV.Models;
+using CSV.Models.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CSV
+{
+    class Program
+    {
+        private static int encryptionDepth = 20;
+        static void Main(string[] args)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+            Console.WriteLine(FTP.DownloadFile(Constants.FTP.BaseUrl+Constants.FTP.mydirectory,desktopPath));
+
+            string filepath = desktopPath+"/info.csv";
+            string fileContents;
+
+            using (StreamReader stream = new StreamReader(filepath))
+            {
+                fileContents = stream.ReadToEnd();
+            }
+
+            List<String> entries = new List<string>();
+            entries = fileContents.Split("\r\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+            String[] Data = entries[1].Split(',');
+            
+
+            String encryptionKey = Data[1];
+
+            int[] key = new int[5];
+            int i = 0;
+            foreach (char c in encryptionKey)
+            {
+                key[i] = System.Convert.ToInt32(c);
+                i = i + 1;
+            }
+            //Encryption Decryption
+            string myString = String.Join(",", key.Select(x => x.ToString()));
+            Console.WriteLine(myString);
+
+
+       
+            string[] nameDeepEncryptWithCipher = new string[5];
+            i = 0;
+
+            foreach (string letter in Data)
+            {
+                Encrypter encrypter = new Encrypter(letter, key, encryptionDepth);
+                //Deep Encrytion
+                nameDeepEncryptWithCipher[i] = Encrypter.DeepEncryptWithCipher(letter, key, encryptionDepth);
+                Console.WriteLine($"\nDeep Decrypted {encryptionDepth} times using the cipher {Data[i]} {nameDeepEncryptWithCipher[i]}");
+                i = i + 1;
+            }
+
+            string finalstring = String.Join(",", nameDeepEncryptWithCipher);
+            var csv = new StringBuilder();
+
+            csv.AppendLine(finalstring);
+
+            File.WriteAllText(desktopPath, csv.ToString());
+        }
+
+    }
+}
